0acab2902504e4f1b8850d9f156dd6ccff58d24d
mhmmdd <mhmmd.dd@gmail.com>
2020-07-22 05:34:48 +0300

    add custom error for arguments

--- pgrep/README.md
+++ pgrep/README.md
@@ -1,1 +1,13 @@
 `$ cargo run -- Hello -f test_data/t1.txt`
+
+
+## To see the errors
+`$ cargo run`
+error: The following required arguments were not provided:
+    <pattern>
+
+USAGE:
+    pgrep.exe [OPTIONS] <pattern>
+
+`$ cargo run Hello`
+Error: ArgErr { arg: "file" }
--- pgrep/src/main.rs
+++ pgrep/src/main.rs
@@ -1,24 +1,39 @@
 use clap::{clap_app, crate_version};
 use regex::Regex;
 use std::path::Path;
+use failure::{Error, Fail};
+use std::fmt;
+use failure::_core::fmt::Formatter;
+
+#[derive(Debug)]
+struct ArgErr {
+    arg: &'static str
+}
+
+impl Fail for ArgErr {}
+
+impl fmt::Display for ArgErr {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f, "Argument Not provided: {}", self.arg)
+    }
+}
 
 #[derive(Debug)]
 struct Record {
     line: usize,
-    tx: String,
+    text: String,
 }
 
-fn process_file<P: AsRef<Path>>(p: P, re: Regex) -> Result<Vec<Record>, String> {
+fn process_file<P: AsRef<Path>>(p: P, re: Regex) -> Result<Vec<Record>, Error> {
     let mut res = Vec::new();
-    let bts = std::fs::read(p)
-        .map_err(|e| "Could not read string".to_string())?;
+    let bts = std::fs::read(p)?;
 
     if let Ok(ss) = String::from_utf8(bts) {
         for (i, l) in ss.lines().enumerate() {
             if re.is_match(l) {
                 res.push(Record {
                     line: i,
-                    tx: l.to_string(),
+                    text: l.to_string(),
                 });
             }
         }
@@ -26,20 +41,19 @@ fn process_file<P: AsRef<Path>>(p: P, re: Regex) -> Result<Vec<Record>, String> 
     Ok(res)
 }
 
-fn main() -> Result<(), String> {
+fn main() -> Result<(), Error> {
     let cp = clap_app!(pgrep =>
         (version: crate_version!())
         (about: "A Grep like program")
-        (author: "Matt")
+        (author: "Muhammed")
         (@arg pattern: +required "The regex pattern to search for")
         (@arg file: -f --file +takes_value "The file to test")
     ).get_matches();
 
-    let re = Regex::new(cp.value_of("pattern").unwrap())
-        .map_err(|_| "bad_regex")?;
+    let re = Regex::new(cp.value_of("pattern").unwrap())?;
 
     let p = process_file(cp.value_of("file")
-                             .ok_or("No file chosen")?, re);
+                             .ok_or(ArgErr { arg: "file" })?, re);
 
 
     println!("{:?}", p);
